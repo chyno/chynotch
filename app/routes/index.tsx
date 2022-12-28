8471acc79ef57a65f73b6d8d1322896614310a52
chyno <jchynowe1@yahoo.com>
2022-12-27 21:58:39 -0500

    add header and footer

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,50 +1,42 @@
 import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
 
 export default function Index() {
-  const user = useOptionalUser();
-  return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-        {user ? (
-          <Link
-            to="/notes"
-            className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-          >
-            View Notes for {user.email}
-          </Link>
-        ) : (
-          <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-            <Link
-              to="/join"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-            >
-              Sign up
-            </Link>
-            <Link
-              to="/login"
-              className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-            >
-              Log In
-            </Link>
-          </div>
-        )}
-      </div>
-      <div> Hello from main
 
-        
+  return (<>
+    {/*  Create a tailwind blog for John Chynoweth email jwchynoweth@gmail */}
+   
+   {/* Create Header tag */}
+    <header className="bg-gray-900">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-white">
+          John Chynoweth
+        </h1>
       </div>
-<div>
-<div className="mx-auto mt-16 max-w-7xl text-center">
-  <Link
-    to="/posts"
-    className="text-xl text-blue-600 underline"
-  >
-    Blog Posts
-  </Link>
-</div>
-</div>
-    </main>
+    </header>
+    {/* Create Main content */}
+    <main>
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        {/* Show the latest post */}
+        <div className="px-4 py-6 sm:px-0">
+          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+        </div>
+        </div>
+        </main>
+        {/* Create the footer */}
+        <footer className="bg-gray-900">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div className="mt-10 flex items-center justify-center text-sm text-white">
+              <div className="ml-6">
+                <p className="text-base leading-6 text-gray-400">
+                  &copy; 2022 John Chynoweth. All rights reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+        
+
+  </>
   );
 }
