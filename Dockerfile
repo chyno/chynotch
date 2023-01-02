10012f012add983ff48d70a2ce7cab8d4d31696c
chyno <jchynowe1@yahoo.com>
2023-01-02 13:15:03 -0500

    app works

--- Dockerfile
+++ Dockerfile
@@ -59,13 +59,4 @@ COPY --from=build /app/start.sh /app/start.sh
 COPY --from=build /app/prisma /app/prisma
 
 ENTRYPOINT [ "./start.sh" ]
-# COPY --from=production-deps /app/node_modules /app/node_modules
-
-# # Uncomment if using Prisma
-#  COPY --from=build /app/node_modules/.prisma /app/node_modules/.prisma
-
-# COPY --from=build /app/build /app/build
-# COPY --from=build /app/public /app/public
-# ADD . .
-
-# CMD ["npm", "run", "start"]
+ 
--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,59 @@
+import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Chynoweth Blog",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export async function loader({ request }: LoaderArgs) {
+  return json({
+    user: await getUser(request),
+  });
+}
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-       
+        <Meta />
+        <Links />
       </head>
       <body>
         <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
-        hello from chyno blog
+          <Header></Header>
+          <div className="lg:grid lg:grid-cols-[1fr_1600px_1fr]">
+            <div className="bg-gray-100"></div>
+
+            <div className="mx-3">
+              <Outlet />
+            </div>
+            <div className="bg-gray-100"></div>
+          </div>
+
+          <Footer></Footer>
         </div>
-        
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
